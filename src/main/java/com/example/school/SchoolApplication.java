8e0f8b04bb16272905f80ca6e766c1ba225e10a0
admin <admin@DESKTOP-88HLS6I>
2018-12-05 00:10:57 +0800

    项目搭建

---
+++ src/main/java/com/example/school/SchoolApplication.java
@@ -0,0 +1,21 @@
+package com.example.school;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @param:
+ * @return:
+ * @author: 
+ * @version 1.0
+ * @date: 2018/8/23 23:37
+ */
+@SpringBootApplication
+public class SchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolApplication.class, args);
+    }
+}
