973054d7fd3e92190244310944ffdd6cea459d7c
fly <1779905848@qq.com>
2018-12-18 01:11:06 +0800

    视频上传 -- zjw

--- src/main/java/com/example/school/monitor/business/index/InitIndexBusiness.java
+++ src/main/java/com/example/school/monitor/business/index/InitIndexBusiness.java
@@ -7,17 +7,20 @@ import java.util.List;
 public interface InitIndexBusiness {
 
     /**
+     * TODO : 根据视频分类名称查询视频,根据日期倒序排列
      * @param name  分类名称
      * @param limit 查几条
-     * @return
+     * @return [ LIST(VIDEO) ]
+     * @author admin
      */
     public List<Video> queryVideoByCatAndLimitOrderByDateDesc( String name , Integer limit );
 
     /**
-     *
+     * TODO : 根据视频分类名称查询视频,根据播放次数倒序排序
      * @param name  分类名称
      * @param limit 查几条
-     * @return
+     * @return [ LIST(VIDEO) ]
+     * @author admin
      */
     public List<Video> queryVideoByCatAndLimitOrderByPlaySumDesc( String name , Integer limit );
 
